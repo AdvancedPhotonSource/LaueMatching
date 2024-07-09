f58f9df6462bec61d7a4c65ee77d94a84581e4ee
Hemant Sharma <hemant@sharmas.biz>
2024-07-09 18:05:57 -0500

    Debug

--- src/LaueMatchingHeaders.h
+++ src/LaueMatchingHeaders.h
@@ -27,30 +27,30 @@
 #define hc_keVnm 1.2398419739
 #define EPS 1E-12
 
-// inline void calcV(double LatC[6]);
-// inline void calcRecipArray(double Lat[6], int SpaceGroup, double recip[3][3]);
-// inline double calcOverlap(double *image, double euler[3], int *hkls, int nhkls, int nrPxX, int nrPxY,
-// 	double recip[3][3], double *outArrThis, int maxNrSpots, double rotTranspose[3][3], double pArr[3], double pxX,
-// 	double pxY, double Elo, double Ehi);
-// inline double problem_function(unsigned n, const double *x, double *grad, void* f_data_supplied);
-// inline double FitOrientation(double *image, double euler[3], int *hkls, int nhkls, int nrPxX, int nrPxY,
-// 	double recip[3][3], double *outArrThis, int maxNrSpots, double rotTranspose[3][3], double pArr[3], double pxX, 
-// 	double pxY, double Elo, double Ehi, double tol, double latc[6], double eulerFit[3], double latCUpd[6], double *minVal, int doCrystalFit);
-// inline int writeCalcOverlap(double *image, double euler[3], int *hkls, int nhkls, int nrPxX, int nrPxY,
-// 	double recip[3][3], double *outArrThis, int maxNrSpots, double rotTranspose[3][3], double pArr[3], double pxX,
-// 	double pxY, double Elo, double Ehi, FILE *ExtraInfo, int saveExtraInfo, int *simulNrSps);
-// inline void MatrixMultF(double m[3][3],double v[3],double r[3]);
-// inline double zeroOut(double val);
-// inline void MatrixMultF33(double m[3][3],double n[3][3],double res[3][3]);
-// inline void Euler2OrientMat(double Euler[3], double m_out[3][3]);
-// inline void OrientMat2Euler(double m[3][3],double Euler[3]);
-// inline void OrientMat2Quat33(double OM[3][3], double Quat[4]);
-// inline void OrientMat2Quat(double OrientMat[9], double Quat[4]);
-// inline double GetMisOrientation(double quat1[4], double quat2[4], int SGNr);
-// inline void BringDownToFundamentalRegion(double QuatIn[4], double QuatOut[4],int SGNr);
-// inline int MakeSymmetries(int SGNr, double Sym[24][4]);
-// inline void QuaternionProduct(double q[4], double r[4], double Q[4]);
-// inline void normalizeQuat(double quat[4]);
+inline void calcV(double LatC[6]);
+inline void calcRecipArray(double Lat[6], int SpaceGroup, double recip[3][3]);
+inline double calcOverlap(double *image, double euler[3], int *hkls, int nhkls, int nrPxX, int nrPxY,
+	double recip[3][3], double *outArrThis, int maxNrSpots, double rotTranspose[3][3], double pArr[3], double pxX,
+	double pxY, double Elo, double Ehi);
+inline double problem_function(unsigned n, const double *x, double *grad, void* f_data_supplied);
+inline double FitOrientation(double *image, double euler[3], int *hkls, int nhkls, int nrPxX, int nrPxY,
+	double recip[3][3], double *outArrThis, int maxNrSpots, double rotTranspose[3][3], double pArr[3], double pxX, 
+	double pxY, double Elo, double Ehi, double tol, double latc[6], double eulerFit[3], double latCUpd[6], double *minVal, int doCrystalFit);
+inline int writeCalcOverlap(double *image, double euler[3], int *hkls, int nhkls, int nrPxX, int nrPxY,
+	double recip[3][3], double *outArrThis, int maxNrSpots, double rotTranspose[3][3], double pArr[3], double pxX,
+	double pxY, double Elo, double Ehi, FILE *ExtraInfo, int saveExtraInfo, int *simulNrSps);
+inline void MatrixMultF(double m[3][3],double v[3],double r[3]);
+inline double zeroOut(double val);
+inline void MatrixMultF33(double m[3][3],double n[3][3],double res[3][3]);
+inline void Euler2OrientMat(double Euler[3], double m_out[3][3]);
+inline void OrientMat2Euler(double m[3][3],double Euler[3]);
+inline void OrientMat2Quat33(double OM[3][3], double Quat[4]);
+inline void OrientMat2Quat(double OrientMat[9], double Quat[4]);
+inline double GetMisOrientation(double quat1[4], double quat2[4], int SGNr);
+inline void BringDownToFundamentalRegion(double QuatIn[4], double QuatOut[4],int SGNr);
+inline int MakeSymmetries(int SGNr, double Sym[24][4]);
+inline void QuaternionProduct(double q[4], double r[4], double Q[4]);
+inline void normalizeQuat(double quat[4]);
 
 struct dataFit{
 	double *image;
