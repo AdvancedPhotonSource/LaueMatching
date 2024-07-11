039dfb3909131cb0b77e8e7eaa2119bdb66cd157
Hemant Sharma <hemant@sharmas.biz>
2024-07-11 01:13:47 -0500

    Minor edits, also added automated download of 7GB Orientation file.

--- getPackages.sh
+++ getPackages.sh
@@ -19,3 +19,8 @@ if [ ! -d ${installDir}/NLOPT/include ]; then # Install NLOPT
     cmake --build . -j 8
 	cmake --build . -j 8 --target install
 fi
+
+if [ ! -f $(pwd)/100MilOrients.bin ]; then # Download 100 Million Orientation File
+    echo "Downloading orientation file. ~7GB, might take long."
+    wget -O 100MilOrients.bin https://anl.box.com/shared/static/qhao454ub2nh5t89zymj1bhlxw1q4obu.bin
+fi
