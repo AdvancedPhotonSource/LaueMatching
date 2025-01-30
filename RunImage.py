686ab9e7a2ee1bcf10527755c2d6baa14c4e758f
Hemant Sharma <hemant@sharmas.biz>
2025-01-29 23:01:00 -0600

    Debug

--- RunImage.py
+++ RunImage.py
@@ -366,36 +366,30 @@ def runFile(imageFN):
 			orientationNr+=1
 	#### LETS check which orientations still had left-over spots, write them now.
 	for orientation in orientationInfo:
-		spotThis = spotInfo[spotInfo[:,0]==orientation[0],:]
-		goodSpots = spotThis[spotThis[:,-1]>=threshT/2,:] # This will ensure that we had intensity greater than the original threshold/2 to account for blurring
+		goodSpots = spotInfo[spotInfo[:,0]==orientation[0],:]
 		#### We need to find first which grains have spots found
-		if goodSpots.shape[0] >= minGoodSpots:
-			# Find which labels were already found, remove those spots from list
-			badRows = []
-			for spotNr in range(goodSpots.shape[0]):
-				spot = goodSpots[spotNr]
-				if labels2[int(spot[6])][int(spot[5])]:
-					if labels2[int(spot[6])][int(spot[5])] in label_found:
-						# Remove this spot from the list
-						badRows.append(spotNr)
-			if goodSpots.shape[0] <= len(badRows):
-				continue
-			for spot in goodSpots:
-				# indices = indices_to_text(int(spot[2]),int(spot[3]),int(spot[4]))     # LABEL
-				# ax.text(spot[5]-np.random.randint(0,20),spot[6]-20,indices,c=colors(orientationNr))     # LABEL
-				# if labels2[int(spot[6])][int(spot[5])]:    #### DEL
-				lbl = labels2[int(spot[6])][int(spot[5])]
-				if lbl and lbl not in label_found:
-					label_found.append(lbl)
-			orientation[5] = goodSpots.shape[0]
-			nsols+=1
-			orientation = orientation.reshape(orientation.shape[0],-1).transpose()
-			np.savetxt(outfor,orientation,fmt=fmtout)
-			np.savetxt(outfsp,goodSpots,fmt='%4d\t%3d\t%3d\t%3d\t%3d\t%5d\t%5d\t%9.6f\t%9.6f\t%9.6f\t%7d')
-			# Save an image with the blobs from found spots as open squares and orientation id
-			lbl = 'rID '+str(int(orientationNr))     # LABEL
-			ax.plot(goodSpots[:,5],goodSpots[:,6],'ks', markerfacecolor='none', ms=3, markeredgecolor=colors(orientationNr),markeredgewidth=0.3,label=lbl)     # LABEL
-			orientationNr+=1
+		badRows = []
+		for spotNr in range(goodSpots.shape[0]):
+			spot = goodSpots[spotNr]
+			if labels2[int(spot[6])][int(spot[5])]:
+				if labels2[int(spot[6])][int(spot[5])] in label_found:
+					# Remove this spot from the list
+					badRows.append(spotNr)
+		if goodSpots.shape[0] <= len(badRows):
+			continue # We already found this grain or all the spots, not writing again
+		for spot in goodSpots:
+			lbl = labels2[int(spot[6])][int(spot[5])]
+			if lbl and lbl not in label_found:
+				label_found.append(lbl)
+		orientation[5] = goodSpots.shape[0]
+		nsols+=1
+		orientation = orientation.reshape(orientation.shape[0],-1).transpose()
+		np.savetxt(outfor,orientation,fmt=fmtout)
+		np.savetxt(outfsp,goodSpots,fmt='%4d\t%3d\t%3d\t%3d\t%3d\t%5d\t%5d\t%9.6f\t%9.6f\t%9.6f\t%7d')
+		# Save an image with the blobs from found spots as open squares and orientation id
+		lbl = 'rID '+str(int(orientationNr))     # LABEL
+		ax.plot(goodSpots[:,5],goodSpots[:,6],'ks', markerfacecolor='none', ms=3, markeredgecolor=colors(orientationNr),markeredgewidth=0.3,label=lbl)     # LABEL
+		orientationNr+=1
 
 	plt.legend()     # LABEL
 	plt.savefig(imageFN+'.bin.LabeledImage.tif',dpi=outdpi)     # LABEL
